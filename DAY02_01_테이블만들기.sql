a972ad5d12d6ca1a7c264e1742b45622c387b0dc
unknown <kimsegong@naver.com>
2023-06-30 10:33:37 +0900

    20230630

--- DAY02_01_테이블만들기.sql
+++ DAY02_01_테이블만들기.sql
@@ -20,3 +20,26 @@
     5. DATE : 날짜와 시간을 동시에 표현하는 타입(년, 월, 일, 시, 분, 초,)
     6. TIMESTAMP : 날짜와 시간을 동시에 표현하는 타입 (년, 월, 일, 시, 분, 초,) 마이크로초
 */
+/*
+    제약조건 5가지
+    1. NOT NULL : 필수
+    2. UNIQUE : 중복 불가
+    3. PRIMARY KEY : 기본키
+    4. FOREIGN KEY : 외래키
+    5. CHECK         : 값의 제한
+    
+*/
+
+DROP TABLE CUSTOMER_A;
+CREATE TABLE CUSTOMER_A (
+    NO      NUMBER NOT NULL PRIMARY KEY,
+    ID       VARCHAR2(32 BYTE) NOT NULL UNIQUE,
+    NAME  VARCHAR2(32 BYTE) NOT NULL,
+    JOB     VARCHAR2(32 BYTE) NULL,
+    PHONE CHAR(13 BYTE) NULL UNIQUE,
+    JUBUN CHAR(14 BYTE) NULL UNIQUE
+);
+    
+    
+    
+    
