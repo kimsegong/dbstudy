e084dc969cfb1aef0ae20d6a8278af95c6fc792b
unknown <kimsegong@naver.com>
2023-07-03 12:36:46 +0900

    20230703

--- DAY03_01_DDL(은행, 고객).sql
+++ DAY03_01_DDL(은행, 고객).sql
@@ -54,6 +54,10 @@ CREATE TABLE CUSTOMER_TBL (
                 ALTER TABLE 자식테이블명 ADD CONSTRAINT 제약조건명 FOREIGN KEY (칼럼) REFERENCES 부모테이블명(참조할칼럼)
             (2) 삭제
                 ALTER TABLE 테이블명 DROP CONSTRAINT 제약조건명
+            (3) 일시중지
+                ALTER TBALE 테이블명 DISABLE CONSTRAINT 제약조건명
+            (4) 다시 시작(활성화)
+                ALTER TABLE 테이블명 ENABLE CONSTRAINT 제약조건명
 
 */
 
